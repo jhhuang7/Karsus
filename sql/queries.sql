2f649803170018fe4cac89c0096edecaea3e9795
jhhuang7 <juhua.huang@uqconnect.edu.au>
2020-09-05 11:55:59 +1000

    Formatting changes

--- sql/queries.sql
+++ sql/queries.sql
@@ -3,19 +3,19 @@ Queries for reference
 */
 
 -- Items purchased by user
-select * from Inventory p where p.name in 
-	(select item from Purchase p inner join Inventory i on p.item = i.name where student = [id])
+select * from Inventory p where p.name in
+                                (select item from Purchase p inner join Inventory i on p.item = i.name where student = [id])
 -- Items not yet purchased
-select * from Inventory p where p.name not in 
-	(select item from Purchase p inner join Inventory i on p.item = i.name where student = [id])
+select * from Inventory p where p.name not in
+                                (select item from Purchase p inner join Inventory i on p.item = i.name where student = [id])
 
 --purchase item
 insert into Purchase values([id], [item_name])
 
 --get balance (This is probably inefficient but I don't think it matters too much)
 select  SUM(score)/COUNT(*) - SUM(cost)
-as [Balance]
+            as [Balance]
 from Students
-inner join Purchase on Students.id = Purchase.student
-inner join Inventory on Purchase.item = Inventory.name
+         inner join Purchase on Students.id = Purchase.student
+         inner join Inventory on Purchase.item = Inventory.name
 where Students.id = [id]
