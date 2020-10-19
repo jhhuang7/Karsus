50effde0814cc890a3bf652530468df1d8b6d313
= <w.fitzmaurice@yahoo.com>
2020-10-19 12:32:13 +1000

    update dbconnect

--- php/dbconntest.php
+++ php/dbconntest.php
@@ -1,12 +1,47 @@
 <?php
-
 $serverName = "tcp:karsus.database.windows.net,1433";
 $connectionOptions = array(
-    "UID" => "karsus", "PWD" => "K@rth0us",
-    "Database" => "Karsus", "LoginTimeout" => 30,
-    "Encrypt" => 1, "TrustServerCertificate" => 0
+    "database" => "Karsus",
+    "uid" => "karsus",
+    "pwd" => "K@rth0us"
 );
 
+// Establishes the connection
 $conn = sqlsrv_connect($serverName, $connectionOptions);
+if ($conn === false) {
+    die(formatErrors(sqlsrv_errors()));
+}
 
+// Select Query
+$tsql = "SELECT @@Version AS SQL_VERSION";
+
+// Executes the query
+$stmt = sqlsrv_query($conn, $tsql);
+
+// Error handling
+if ($stmt === false) {
+    die(formatErrors(sqlsrv_errors()));
+}
+?>
+
+<h1> Results : </h1>
+
+<?php
+while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+    echo $row['SQL_VERSION'] . PHP_EOL;
+}
+
+sqlsrv_free_stmt($stmt);
+sqlsrv_close($conn);
+
+function formatErrors($errors)
+{
+    // Display errors
+    echo "Error information: <br/>";
+    foreach ($errors as $error) {
+        echo "SQLSTATE: ". $error['SQLSTATE'] . "<br/>";
+        echo "Code: ". $error['code'] . "<br/>";
+        echo "Message: ". $error['message'] . "<br/>";
+    }
+}
 ?>
